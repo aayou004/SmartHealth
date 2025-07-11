7d2aa70d996eec9922ed4ca3a992aa3efa69e8cd
Augustine Ayoub <ayoubraugustine@gmail.com>
2025-07-10 18:52:51 -0700

    got prototype frontend/backend working

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-4xl text-blue-600">
+      Tailwind CSS is working! 🚀
+    </div>
+  );
+}
+
+export default App;
